5191cddef2e5cc5a2e7d50504f7e069ba342007b
Cancu <juancruz.cancu@gmail.com>
2022-09-17 14:15:02 -0300

    added font and ui changes

--- pages/canva.js
+++ pages/canva.js
@@ -6,9 +6,14 @@ const CanvasEditor = () => {
   const [currentColor, setCurrentColor] = useState("#FFF");
 
   return (
-    <div className=''>
-        <Options handleColor={setCurrentColor}/>
-        <Map currentColor={currentColor} />
+    <div className='flex flex-col justify-center items-center'>
+        <div className='flex justify-center items-center'>
+          <Map currentColor={currentColor} />
+        </div>
+        <div className='flex items-center justify-center'>
+          <h2 className='text-black font-barlow text-2xl'>Add a color</h2>
+          <input type="color" onChange={(e) => setCurrentColor(e.target.value)} />
+        </div>
     </div>
   );
 }
